9e2c719ac065c5977ae4885def1e67ebd493bc6a
Chantelle G. Dubois <c.g.dubois@protonmail.com>
2023-12-30 17:11:02 -0500

    Added processor to diviner_tools

--- diviner_tools.py
+++ diviner_tools.py
@@ -5,14 +5,12 @@
 # @file diviner_tools.py
 # @author Chantelle G. Dubois (chantelle.dubois@mail.concordia.ca)
 #	
-
 from bs4 import BeautifulSoup
 import concurrent.futures
 from enum import Enum
 import os
 import requests
 import re
-import subprocess
 import sqlite3
 from urllib.parse import urljoin
 from zipfile import ZipFile
@@ -38,7 +36,7 @@ class DivinerTools(object):
 		self.__createDatabase(db_filepath)
 
 	def __createDir(self, data_dir):
-		"""! Creates project directory if it doesn't already exist.
+		"""! Creates project directory if it doesn't already exist
 
 		@param data_dir The data directory filepath
 		"""
@@ -48,7 +46,7 @@ class DivinerTools(object):
 	def __createDatabase(self, db_filepath):
 		"""! Create database if it doesn't exist
 
-		@param db_filepath The filepath to the database object.
+		@param db_filepath The filepath to the database object
 		"""
 		db_connection = sqlite3.connect(db_filepath)
 
@@ -105,11 +103,11 @@ class DivinerTools(object):
 
 
 	def get_sub_urls(self, parent_url, pattern=None):
-		"""! Returns a list of sub-links on a parent page.
+		"""! Returns a list of sub-links on a parent page
 
-		@param parent_url The url page that is being searched.
-		@param pattern A regex pattern if required to filter the url list.
-		@return A list of sub-links on the page.
+		@param parent_url The url page that is being searched
+		@param pattern A regex pattern if required to filter the url list
+		@return A list of sub-links on the page
 		"""
 
 		# Send a GET request to get page elements
@@ -153,8 +151,8 @@ class DivinerTools(object):
 		# lroldr_1001 contains data from 2009 - 2016
 		# lroldr_1002 contains data from 2017 - 2023
 		parent_urls = [
-		'https://pds-geosciences.wustl.edu/lro/lro-l-dlre-4-rdr-v1/lrodlr_1001/data/',
-		'https://pds-geosciences.wustl.edu/lro/lro-l-dlre-4-rdr-v1/lrodlr_1002/data/']
+			'https://pds-geosciences.wustl.edu/lro/lro-l-dlre-4-rdr-v1/lrodlr_1001/data/',
+			'https://pds-geosciences.wustl.edu/lro/lro-l-dlre-4-rdr-v1/lrodlr_1002/data/']
 
 		# Regex pattern will filter URLs for years
 		# If a year is specified, the search is only for that year
@@ -233,7 +231,7 @@ class DivinerTools(object):
 
 
 	def insert_into_database(self, dest_db, data):
-		"""! Adds a Diviner RDR LVL1 data line into a target database.
+		"""! Adds a Diviner RDR LVL1 data line into a target database
 
 		@param dest_db The pathway to the destination database
 		@param data The text line containing the data entry
@@ -311,7 +309,7 @@ class DivinerTools(object):
 
 
 	def append_to_file(self, filepath, data):
-		"""! Appends data to target text file.
+		"""! Appends data to target text file
 
 		@param filepath The text file path
 		@param data The data to be appended
@@ -336,3 +334,43 @@ class DivinerTools(object):
 			lines = [line.strip() for line in file.readlines()]
 
 		return lines
+
+	def processor(self, url, tab_dir, db_filepath, useful_tab_file):
+		"""! Preprocesses RDR data tables all the way from download
+			to writing to the database
+
+		@param url The url of the .zip file containing RDR data
+		@param tab_dir The directory to save the files
+		@param db_filepath The filepath to the database
+		@param tab_txt_filepath The filepath of a text file to 
+			keep track of useful filenames
+		"""
+		self.download_unpack_delete(tab_dir, url)
+
+		# Synth filename from url
+		file = re.search(r'(\d{12}_rdr)', url)[0].upper()
+		filename = os.path.join(tab_dir,  file + ".TAB")
+
+		# Read lines from .TAB file
+		lines = self.tab_to_lines(filename)
+
+		# Process each line and add to database it qualifies
+		# Note: if we use multithreading, sqlite3 doesn't
+		# support concurrent writes, but it's supposed to
+		# somehow queue write requests
+		count = 0
+    
+		for line in lines:
+			count += self.insert_into_database(db_filepath, line)
+
+		# Since there are files that contain no target 
+		# data, we want to keep track of the ones that do
+		# so that in the future we don't have to download
+		# every RDR file
+		if (count > 0):
+			data = file + " " + count
+			self.append_to_file(useful_tab_file, data)
+
+		# We no longer need the .TAB data and will delete
+		# it to preserve storage space
+		os.remove(filename)
