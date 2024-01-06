5e706c0788dd9ec92c8473ab410f92565115e635
Chantelle G. Dubois <c.g.dubois@protonmail.com>
2024-01-06 15:32:38 -0500

    Removed Notebooks, added find_urls.py

---
+++ scripts/find_urls.py
@@ -0,0 +1,39 @@
+'''
+@file find_urls.py
+@author Chantelle G. Dubois (chantelle.dubois@mail.concordia.ca)
+ 
+@brief	We will extract the URLs for the zip files which contain tab files that contain the RDR LVL1 tables. 
+		This can be a somewhat slow process, so we will do this once and then save the urls to a text file. 
+		Skip this step if the ZIP_URLS_FILE already exists and has been populated. We expect 717,509 URLs.
+
+		Each year takes approximately 30-45 seconds, so total time should be around 5-10 minutes.
+'''
+from diviner_tools import DivinerTools
+
+def main()
+
+	# Get CFG_FILEPATH and ZIP_FILEPATH from args
+	if len(sys.argv) < 4:
+		print("CFG_FILEPATH and ZIP_FILEPATH required")
+		sys.exit(1)
+	
+	# Filepath to the yaml config file
+	CFG_FILEPATH = sys.argv[1]
+
+	# Filepath to list of .zip URLs
+	ZIP_FILEPATH = sys.argv[2]
+
+	# Init Diviner Tools
+	dt = DivinerTools(CFG_FILEPATH)
+
+	# Extract .zip URLs
+	zip_urls = dt.find_all_zip_urls()
+	print("Found " + repr(len(zip_urls)) + " .zip file urls")
+
+	# Save URLs to file
+	append_to_file(ZIP_FILEPATH, zip_urls)
+	print("Saved to: " + ZIP_FILEPATH)
+
+
+if __name__ == '__main__':
+	main()
