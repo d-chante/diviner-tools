797a83ff2aa7fa73df05aff321ca52b3c7860e58
Chantelle G. Dubois <c.g.dubois@protonmail.com>
2024-01-19 23:55:02 -0500

    Added docker files

---
+++ support/docker/interactive.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -it --entrypoint /bin/bash -v /Users/cactusisland/Desktop/diviner-tools:/diviner-tools -v /Users/cactusisland/Desktop/esthar:/esthar diviner-tools
---
+++ support/docker/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -v /Users/cactusisland/Desktop/diviner-tools:/diviner-tools -v /Users/cactusisland/Desktop/esthar diviner-tools
