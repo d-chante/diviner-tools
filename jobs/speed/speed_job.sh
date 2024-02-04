9d48440f834b1f1fed3deb0a1517404a2c0ee35c
Chantelle G. Dubois <chantelle.dubois@mail.concordia.ca>
2024-02-04 14:47:19 -0500

    Simplified speed job scripts and merged env setup with each job

--- jobs/speed/speed_job.sh
+++ jobs/speed/speed_job.sh
@@ -1,6 +1,5 @@
 #!/encs/bin/tcsh
 
-#SBATCH --job-name=7_diviner_preprocess
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=chantelle.dubois@mail.concordia.ca
 #SBATCH --chdir=/speed-scratch/d_chante/tmp
@@ -10,17 +9,35 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=15G
 
+# Set job id passed to script
+set job_id = $JOB_ID
+
 # Load anaconda module
 module load anaconda3/default
 
+# Init conda shell
+conda init tcsh
+
+# Re-source shell
+source ~/.tcshrc
+
+# Create conda env
+conda create --prefix /speed-scratch/d_chante/env/dte${job_id} --yes
+
 # Activate conda env
-conda activate /speed-scratch/d_chante/env/dte
+conda activate /speed-scratch/d_chante/env/dte${job_id}
+
+# Install pip
+conda install pip
+
+# Install pcakages from requirements.txt
+pip install -r /speed-scratch/d_chante/diviner-tools/support/other/requirements.txt
 
 # Run preprocessing job
 python3 /speed-scratch/d_chante/diviner-tools/scripts/data_preprocess.py \
     /speed-scratch/d_chante/diviner-tools/config/speed_cfg.yaml \
     /speed-scratch/d_chante/diviner-tools/support/other/zip_urls.txt \
-    7
+    ${job_id}
 
 # Deactivate the environment
 conda deactivate
---
+++ jobs/speed/start_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Capture the job_id argument
+job_id="$1"
+
+# Generate job name
+job_name=${job_id}_diviner_preprocess
+
+# Use --export to pass the JOB_ID variable to the script environment and set the job name
+sbatch --job-name=${job_name} --export=JOB_ID=$job_id speed_job.sh
