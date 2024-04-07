42c8f5ec06cc3625fde61099f324d2061b62c123
Chantelle G. Dubois <c.g.dubois@protonmail.com>
2024-04-07 15:33:17 -0400

    Add create_indices_job

---
+++ jobs/speed/create_indices_job.sh
@@ -0,0 +1,30 @@
+#!/encs/bin/tcsh
+
+#SBATCH --job-name=create_indices_diviner
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=chantelle.dubois@mail.concordia.ca
+#SBATCH --chdir=/speed-scratch/d_chante/tmp
+#SBATCH --time=3-00:00:00
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+
+# Load anaconda module
+module load anaconda3/2023.03/default
+
+# Logging env
+env
+
+# Activate conda env
+conda activate /speed-scratch/d_chante/env/dte
+
+# Run preprocessing job
+python3 /speed-scratch/d_chante/diviner-tools/scripts/create_indices.py \
+    /speed-scratch/d_chante/diviner_data/database
+
+# Deactivate the environment
+conda deactivate
+
+# End job
+exit
