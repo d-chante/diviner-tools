16a7fc1b4a60a7d98d983db80174c7994e602d58
Chantelle G. Dubois <c.g.dubois@protonmail.com>
2024-04-07 15:31:25 -0400

    Add create_indices script

---
+++ scripts/create_indices.py
@@ -0,0 +1,37 @@
+'''
+@file create_indices.py
+@author Chantelle G. Dubois (chantelle.dubois@mail.concordia.ca)
+
+@brief	This python script will create coordinate and datetime
+    indices for the target databases
+'''
+from diviner_tools import DatabaseTools
+import glob
+import os
+import sys
+
+def main():
+
+    # Get DB_DIR from args
+    if len(sys.argv) < 2:
+        print("DB_DIR required")
+        sys.exit(1)
+
+    # Directory to databases
+    DB_DIR = sys.argv[1]
+
+    # Create list of .db files in DB_DIR
+    pattern = os.path.join(DB_DIR, '*.db')
+    db_files = glob.glob(pattern)
+
+    # For each db, create indices:
+    #   - idx_clat_clon
+    #   - idx_datetime
+    dbt = DatabaseTools()
+
+    for db in db_files:
+        dbt.createCoordinateIndex(db)
+        dbt.createDatetimeIndex(db)
+    
+if __name__ == "__main__":
+    main()
