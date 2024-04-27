4064b80d5c13ac29ed31707ce3bab13652ec5d11
Chantelle Dubois <chantelle.dubois@live.concordia.ca>
2024-04-27 13:55:12 -0400

    Remove docker/

--- support/docker/interactive.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --entrypoint /bin/bash -v /Users/cactusisland/Desktop/diviner-tools:/diviner-tools -v /Users/cactusisland/Desktop/esthar:/esthar diviner-tools
--- support/docker/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -v /Users/cactusisland/Desktop/diviner-tools:/diviner-tools -v /Users/cactusisland/Desktop/esthar diviner-tools
